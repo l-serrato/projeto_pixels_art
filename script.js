db7698f1699bee237f56cf8322c6184c2d8b9135
Luis <l-serrato@live.com>
2022-10-04 19:36:41 -0300

    9

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function changeColor(){
       random.addEventListener("click", setBg);
       setBg();
 }
-//localStorage.setItem('colorPalette', JSON.stringify(changeColor));
-//localStorage.getItem('colorPalette'); 
+localStorage.setItem('colorPalette', JSON.stringify(changeColor));
+localStorage.getItem('colorPalette'); 
 const boxes = document.querySelectorAll('td');
 
 for (const box of boxes) {
@@ -21,3 +21,13 @@ for (const box of boxes) {
 }
 document.getElementById('black').classList.add('selected');
 
+const paleta = document.getElementById('color-palette');
+paleta.addEventListener('click', (event) => {
+    const colors = document.querySelectorAll('.color');
+    for (let i = 0; i < colors.length; i += 1) {
+      if (colors[i].classList.contains ('selected')) {
+         colors[i].classList.remove ('selected');
+        }
+        event.target.classList.add ('selected');
+        }
+}); 
